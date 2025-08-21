b889d12a873b8b6d479eeac2c069db43b94b51ab
Deniz Aydemir <deniz@aydemir.us>
2025-08-21 18:50:11 +0300

    chore: bump version to 0.0.14.post1

--- sensing_garden_client/__init__.py
+++ sensing_garden_client/__init__.py
@@ -4,7 +4,7 @@ Sensing Garden Client
 A Python client for interacting with the Sensing Garden API.
 """
 
-__version__ = "0.0.14"
+__version__ = "0.0.14.post1"
 
 from .client import SensingGardenClient
 
